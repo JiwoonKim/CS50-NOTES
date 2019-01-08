9eea9470c94206dbff595ad7632ea114f062ec68
Jiwoon Kim <diana0501@naver.com>
2019-01-08 11:26:28 +0900

    Create whodunit.c

---
+++ whodunit.c
@@ -0,0 +1,122 @@
+// Copies a BMP file
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: whodunit infile outfile\n");
+        return 1;
+    }
+
+    // remember filenames
+    char *infile = argv[1];
+    char *outfile = argv[2];
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        // iterate over pixels in scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
+
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+            // initialize min_color to 0x00
+            BYTE min_color = 0x00;
+
+            // if blue is the minimum value, set min_color to blue's value
+            if ((triple.rgbtBlue < triple.rgbtGreen) && (triple.rgbtBlue < triple.rgbtRed))
+            {
+                min_color = triple.rgbtBlue;
+            }
+            // if green is the minimum value, set min_color to green's value
+            else if ((triple.rgbtGreen < triple.rgbtRed) && (triple.rgbtGreen < triple.rgbtBlue))
+            {
+                min_color = triple.rgbtGreen;
+            }
+            // if red is the minimum value, set min_color to red's value
+            else if ((triple.rgbtRed < triple.rgbtGreen) && (triple.rgbtRed < triple.rgbtBlue))
+            {
+                min_color = triple.rgbtRed;
+            }
+            // eliminate noise by equalizing the colors to the same values
+            triple.rgbtBlue = min_color;
+            triple.rgbtGreen = min_color;
+            triple.rgbtRed = min_color;
+
+            // write RGB triple to outfile
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+
+        // skip over padding, if any
+        fseek(inptr, padding, SEEK_CUR);
+
+        // then add it back (to demonstrate how)
+        for (int k = 0; k < padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // success
+    return 0;
+}
